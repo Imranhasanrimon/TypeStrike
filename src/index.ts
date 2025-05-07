f3b284eab6d39f4be82f97a90eedb17e7ce7fd5b
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-05-08 01:54:37 +0600

    problem 2 is solved

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ type FormatString = (input: string, toUpper?: boolean) => string;
 const formatString: FormatString = (input, toUpper = true) => {
     return toUpper ? input.toUpperCase() : input.toLowerCase()
 }
-//------------------------------------------------2
 
+
+//------------------------------------------------2
+interface Item {
+    title: string;
+    rating: number;
+}
+const filterByRating = (items: Item[]): Item[] => items.filter(item => item.rating >= 4)
 
 
 //------------------------------------------------3
+
 //------------------------------------------------4
 //------------------------------------------------5
 //------------------------------------------------6
